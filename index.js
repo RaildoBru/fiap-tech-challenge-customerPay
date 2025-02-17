7d4c76293c4de69744d7cf409e7fb04aa03b4094
RaildoBru <raildobruno@gmail.com>
2025-02-16 23:07:34 -0300

    adicionando documentação das apis

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import Pool  from 'pg';
 import bodyParser from "body-parser";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./src/docs/swagger.js";
 
 import  customerRoutes from "./src/routes/customer.routes.js"
 
@@ -21,6 +23,7 @@ app.get('/', (req, res) => {
 
 app.use('/customer', customerRoutes);
 app.use('/payment', paymentCreate)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => console.log(`API Gateway rodando na porta ${PORT}`));
@@ -30,3 +33,25 @@ app.listen(PORT, () => console.log(`API Gateway rodando na porta ${PORT}`));
 //app.listen(PORT, () => {
 //  console.log(`Serviço de clientes rodando na porta ${PORT}`);
 //});
+
+
+/*
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const userRoutes = require("./routes/user.routes");
+const swaggerSpec = require("./docs/swagger");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/users", userRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Swagger disponível em http://localhost:${PORT}/api-docs`);
+});
+ 
+*/
